d55b3fff8d78574523c7e7e073103e9a5cec3981
CLG-CODES <24bit008@sot.pdpu.ac.in>
2025-04-23 09:15:08 +0530

    Update Py lab 12.py

--- Py lab 12.py
+++ Py lab 12.py
@@ -89,3 +89,64 @@ class Q4:
 
 obj1=Q4()   
 obj1.circum(obj1.choice())
+
+#CODE5:Write a program that creates and uses a Time class to perform various time arithmetic operations.
+class Time:
+    to_mins = lambda self: str((self.h)*60+self.m)+" mins";
+    def __init__(self,h,m):
+        self.h=h;self.m=m;
+    timegap = lambda self,others: str(abs(self.h-others.h))+" hours "+str(abs(self.m-others.m))+" mins is the time difference";
+    def convert(self):
+        status="AM";
+        if self.h>12: self.h,status=self.h-12,"PM";
+        return str(self.h)+" "+str(self.m)+" "+status;
+        
+t1=Time(int(input("Give hour input(24H format) 1:  ")),int(input("Give minuite 1 input:  ")));
+t2=Time(int(input("Give hour input(24H format) 2:  ")),int(input("Give minuite  2 input:  ")));
+print("Time 1 in mins:" ,t1.to_mins());
+print("Time 2 in mins:" ,t2.to_mins());
+print("Time gap is:",Time.timegap(t1,t2));
+print("Time 1 in 12H format",Time.convert(t1));
+print("Time 2 in 12H format" ,Time.convert(t2));
+
+
+#CODE6:Write a program to create a class Date that has a list containing day, month and year attributes. Define an overloaded == operator to compare two Date objects.
+class Date:
+    def __init__(self, day, month, year):
+        self.date = [day, month, year];
+    def __eq__(self, other):
+        if isinstance(other, Date):return self.date == other.date;
+        return False;
+date1 = Date(13, 12, 2025);
+date2 = Date(8, 7, 1458);
+print(date1 == date2);
+
+#CODE7:Create a class Weather that has a list containing weather parameters. Define an overloaded in operator that checks whether an item is present in the list. (Hint: define the function __contains__( )in a class.
+class Weather:
+    def __init__(self, temperature, wind):
+        self.weather_params = [temperature, wind];
+
+    def __contains__(self, item):
+        return item in self.weather_params;
+
+weather = Weather(25, True);
+print(25 in weather); 
+
+#CODE8:Implement a String class containing the following functions:a. Overloaded += operator function to perform string concatenationb. Method toLower() to convert upper case letters to lower case.c. Method toUpper() to convert lower case letters to upper case
+class String:
+    def __init__(self, stri=""):
+        self.stri = stri;
+    def __iadd__(self, other):
+        if isinstance(other, String):
+            self.stri += other.stri;
+        else:
+            self.stri += str(other);
+        return self
+    def toLower(self):
+        self.stri = self.stri.lower(); return self.stri;   
+    def toUpper(self):
+        self.stri = self.stri.upper(); return self.stri;
+s1=String("cAt");
+print(s1.toUpper());
+
+
